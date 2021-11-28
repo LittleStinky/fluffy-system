68e20989595810b26789507047a3578b3e56128e
LittleStinky <85828665+LittleStinky@users.noreply.github.com>
2021-11-28 21:36:05 +0300

    Create task2.py

---
+++ homework3/task2.py
@@ -0,0 +1,14 @@
+x = y = 0
+while 1:
+    direction = input("Введите направление: ")
+    if (direction.lower() == "влево"):
+        x -= int(input("Введите число шагов: "))
+    elif (direction.lower() == "вправо"):
+        x += int(input("Введите число шагов: "))
+    elif (direction.lower() == "вверх"):
+        y += int(input("Введите число шагов: "))
+    elif (direction.lower() == "вниз"):
+        y -= int(input("Введите число шагов: "))
+    elif(direction.lower() == "стоп"):
+        break;
+    print(f"({x},{y})")
