2c7bd38b4feee3c6a856a5d9d1c2df3c35c5f4c6
LittleStinky <85828665+LittleStinky@users.noreply.github.com>
2021-12-06 02:35:48 +0300

    Create task1.py

---
+++ homework5/task1.py
@@ -0,0 +1,20 @@
+def is_password(password):
+    if len(password) < 6:
+        print("Пароль должен быть не менее 6 символов")
+        return False
+    if password.isdigit():
+        print("Пароль не должен состоять только из цифр")
+        return False
+    if password.lower().find("password") != -1:
+        print("Пароль не должен содержать слово 'password' в любом регистре")
+        return False
+    for i in range(len(password)):
+        if password[i].isdigit():
+            return True
+    print("Пароль должен содержать хотя бы одну цифру")
+    return False
+
+if is_password(input("Введите пароль:")):
+    print("Пароль принят")
+else:
+    print("Пароль не принят")
