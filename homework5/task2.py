2083de4e46343ff69072e6e75378c4962369096e
LittleStinky <85828665+LittleStinky@users.noreply.github.com>
2021-12-06 11:46:24 +0300

    Create task2.py

---
+++ homework5/task2.py
@@ -0,0 +1,79 @@
+def sum(*args):
+    print(args)
+    if len(args) == 0:
+        rez = 0
+    else:
+        rez = args[0]
+        for i in range(1, len(args)):
+            rez += args[i]
+    return rez
+
+try:
+    print("Сумма равна", sum())
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print("Сумма равна", sum(1, 2, 3))
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print("Сумма равна", sum("ab", "cd"))
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print("Сумма равна", sum((1, 2), (3, 4)))
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print(1, 2, "cd")
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print("Сумма равна", sum( (1, 2), 3, 4))
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print("Сумма равна", sum((1, 2), {3, 4}))
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print("Сумма равна", sum((1, 2), [3, 4]))
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print("Сумма равна", sum((1, 2), "cd"))
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print("Сумма равна", sum( {1, 2}, 3, 4))
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print("Сумма равна", sum({1, 2}, [3, 4]))
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print("Сумма равна", sum({1, 2}, "cd"))
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print("Сумма равна", sum( [1, 2], 3, 4))
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
+
+try:
+    print("Сумма равна", sum([1, 2], "cd"))
+except TypeError:
+    print("Операция применена к объекту несоответствующего типа")
