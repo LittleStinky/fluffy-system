3bc1ba6362c3b2149979fd80f431f051197d3d13
LittleStinky <85828665+LittleStinky@users.noreply.github.com>
2021-12-13 14:30:12 +0300

    Update task1.py

--- homework6/task1.py
+++ homework6/task1.py
@@ -21,7 +21,8 @@ def list_decode(list_byte):
         list_str.append(list_byte[i].decode())
     return list_str
 
-print("Преобразование списка строк в список байт-кодов:", list_encode(list_str = list(input("\nВведите список строк, разделенных запятыми:").split(','))), '\n')
+print("Преобразование списка строк в список байт-кодов:", 
+      list_encode(list_str = list(input("\nВведите список строк, разделенных запятыми:").split(','))), '\n')
 
 new_list_str = (input("Введите список байт кодов, разделенных запятыми:").split(','))
 list_byte = []
@@ -29,4 +30,5 @@ for i in range(len(new_list_str)):
     list_byte.append(bytearray(new_list_str[i], encoding='utf-8'))
 print("Преобразование списка байт-кодов в список строк", list_decode(list_byte), '\n')
 
-print("Проверка применения обратных функций друг к другу:", list_decode(list_encode(list_str = list(input("Введите список строк, разделенных запятыми:").split(',')))))
+print("Проверка применения обратных функций друг к другу:", 
+      list_decode(list_encode(list_str = list(input("Введите список строк, разделенных запятыми:").split(',')))))
