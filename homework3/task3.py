e319b27379d639723d1a180db691d650904771a0
LittleStinky <85828665+LittleStinky@users.noreply.github.com>
2021-12-13 03:15:31 +0300

    Update task3.py

--- homework3/task3.py
+++ homework3/task3.py
@@ -1,16 +1,16 @@
 s = input("Введите коэффициенты квадратного уравнения через пробел: ").split(" ")
-numCoeff = list(float(i) for i in s)
-a = numCoeff[0]
-b = numCoeff[1]
-c = numCoeff[2]
-d = b**2-4*a*c
+num_coeff = list(float(i) for i in s)
+a = num_coeff[0]
+b = num_coeff[1]
+c = num_coeff[2]
+d = b**2 - 4*a*c
 if a == 0:
     print("Это не является квадратным уравнением")
 elif d > 0:
-    x = [(-b+d**(0.5))/(2*a), (-b-d**(0.5))/(2*a)]
-    print(f"Корни квадратного уравнения {x[0]} {x[1]}")
+    x = [(- b + d**(0.5)) / (2*a), (- b - d**(0.5)) / (2*a)]
+    print(f"Корни квадратного уравнения {x[0]}, {x[1]}")
 elif d == 0:
-    x = -b/(2*a)
+    x = - b / (2*a)
     print(f"Корень квадратного уравнения {x}")
 elif d < 0:
     print("Корней нет")
