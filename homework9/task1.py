3008ad71e2496d330d5e9e67fe3eff587b9dd3a5
LittleStinky <85828665+LittleStinky@users.noreply.github.com>
2021-12-20 01:44:33 +0300

    Create task1.py

---
+++ homework9/task1.py
@@ -0,0 +1,20 @@
+def IsPassword(password):
+    """
+    Функция принимает в качестве аргумента строку-пароль.
+    Функция проверяет, подходит ли пароль под условия.
+    Функция возвращает True - если подходит, False не подходит
+    """
+    if len(password) < 6:
+        print("Пароль должен быть не менее 6 символов")
+        return False
+    if password.isdigit():
+        print("Пароль не должен состоять только из цифр")
+        return False
+    if password.lower().find("password") != -1:
+        print("Пароль не должен содержать слово 'password' в любом регистре")
+        return False
+    for i in range(len(password)):
+        if password[i].isdigit():
+            return True
+    print("Пароль должен содержать хотя бы одну цифру")
+    return False
