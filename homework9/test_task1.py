7586fa7d894f66307638a31e5fa5a27535c062b0
LittleStinky <85828665+LittleStinky@users.noreply.github.com>
2021-12-20 01:47:35 +0300

    Update test_task1.py

--- homework9/test_task1.py
+++ homework9/test_task1.py
@@ -5,8 +5,8 @@ class TestIsPassword:
         assert not task1.IsPassword('paSSword')
     def test_000000(self):
         assert not task1.IsPassword('000000')
-    def test_abcdfu(self):
-        assert not task1.IsPassword('abcdfu')  
+    def test_abcdef(self):
+        assert not task1.IsPassword('abcdef')  
     def test_abc(self):
         assert not task1.IsPassword('abc')
     def test_12345t(self):
